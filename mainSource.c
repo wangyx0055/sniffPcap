41e6b2d5f741317e3fdedfeb0e213d180422fb27
surivinta <surivinta@gmail.com>
2015-10-22 04:32:34 +0300

    some fixes

--- mainSource.c
+++ mainSource.c
@@ -17,56 +17,53 @@
 
 #include <arpa/inet.h>
 
-//#include <pcap.h>
+//#include <pcap.h> // moved to header
 #include "sniffer.h"
-pcap_t *pd;
-int linkhdrlen;
-#define SIZE_UDP        8               /* length of UDP header */
 
-/* ethernet headers are always exactly 14 bytes [1] */
-#define SIZE_ETHERNET 14
+#define SIZE_UDP           8  // length of UDP header
+#define SIZE_ETHERNET      14 // size of etherned headers, always has 14 bytes
+#define ETHER_ADDR_LEN     6  // size of ethernet addresses
+
+
+// some macroses
 #define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
 #define IP_V(ip)                (((ip)->ip_vhl) >> 4)
-/* Ethernet addresses are 6 bytes */
-#define ETHER_ADDR_LEN  6
-/*
-void bailout(int signo);
-void parsePacket(u_char *user, struct pcap_pkthdr *packethdr, u_char *packetptr);
-void captureLoop(pcap_t* pd, int packets, pcap_handler func);
-pcap_t* openPcapSocet(char *device, const char *bpfstr);
-*/
 
+
+pcap_t *pd;
+
+int linkhdrlen;
+
+// udp struct header
 struct sniff_udp {
-         u_short uh_sport;               /* source port */
-         u_short uh_dport;               /* destination port */
-         u_short uh_ulen;                /* udp length */
-         u_short uh_sum;                 /* udp checksum */
-
+         u_short uh_sport;  // source port
+         u_short uh_dport;  // destination port
+         u_short uh_ulen;   // udp length
+         u_short uh_sum;    // udp checksum
 };
 
-
-/* IP header */
+// ip struct header
 struct sniff_ip {
-        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
-        u_char  ip_tos;                 /* type of service */
-        u_short ip_len;                 /* total length */
-        u_short ip_id;                  /* identification */
-        u_short ip_off;                 /* fragment offset field */
-        #define IP_RF 0x8000            /* reserved fragment flag */
-        #define IP_DF 0x4000            /* dont fragment flag */
-        #define IP_MF 0x2000            /* more fragments flag */
-        #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
-        u_char  ip_ttl;                 /* time to live */
-        u_char  ip_p;                   /* protocol */
-        u_short ip_sum;                 /* checksum */
-        struct  in_addr ip_src,ip_dst;  /* source and dest address */
+        u_char  ip_vhl;                 // version << 4 | header length >> 2
+        u_char  ip_tos;                 // type of service
+        u_short ip_len;                 // total length
+        u_short ip_id;                  // identification
+        u_short ip_off;                 // fragment offset field
+        #define IP_RF 0x8000            // reserved fragment flag
+        #define IP_DF 0x4000            // dont fragment flag
+        #define IP_MF 0x2000            // more fragments flag
+        #define IP_OFFMASK 0x1fff       // mask for fragmenting bits
+        u_char  ip_ttl;                 // time to live
+        u_char  ip_p;                   // protocol
+        u_short ip_sum;                 // checksum
+        struct  in_addr ip_src,ip_dst;  // source and dest address
 };
 
-/* Ethernet header */
+// ethernet struct header
 struct sniff_ethernet {
-        u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
-        u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
-        u_short ether_type;                     /* IP? ARP? RARP? etc */
+        u_char  ether_dhost[ETHER_ADDR_LEN];    // destination host address
+        u_char  ether_shost[ETHER_ADDR_LEN];    // source host address
+        u_short ether_type;                     // IP? ARP? RARP? etc
 };
 
 
@@ -75,10 +72,10 @@ int main(int argc, char **argv)
     char interface[256] = "";
     char bpfstr[256] = "";
     int packets = 0;
-    int c, i;
+//    int c, i;
 
     // get cmd line options
-    while ((c = getopt (argc, argv, "hi:n:")) != -1)
+   /* while ((c = getopt (argc, argv, "hi:n:")) != -1)
     {
         switch (c)
         {
@@ -93,30 +90,31 @@ int main(int argc, char **argv)
             packets = atoi(optarg);
             break;
         }
-    }
+    }*/
 
     // get the packet capture filter expr
-    for (i = optind; i < argc; i++)
+   /* for (i = optind; i < argc; i++)
     {
         strcat(bpfstr, argv[i]);
         strcat(bpfstr, " ");
-    }
+    }*/
 
     // open libpcac, sets signal and start capture
     if ((pd = openPcapSocet(interface, bpfstr)))
     {
-        signal(SIGINT, bailout);
-        signal(SIGTERM, bailout);
-        signal(SIGQUIT, bailout);
+        //signal(SIGINT, bailout);
+        //signal(SIGTERM, bailout);
+        //signal(SIGQUIT, bailout);
         captureLoop(pd, packets, (pcap_handler)parsePacket);
     }
+    pcap_close(pd);
     return 0;
 }
 
 pcap_t* openPcapSocet(char *device, const char *bpfstr)
 {
     char errBuf[PCAP_ERRBUF_SIZE];
-    pcap_t *pd;
+    //pcap_t *pd; // del?
     uint32_t srcIp, netmask;
     struct bpf_program bpf;
 
@@ -152,7 +150,7 @@ pcap_t* openPcapSocet(char *device, const char *bpfstr)
         printf("pcap_setfilter(): %s\n", pcap_geterr(pd));
         return NULL;
     }
-
+    pcap_freecode(&bpf);
     return pd;
 }
 
@@ -195,35 +193,27 @@ void captureLoop(pcap_t* pd, int packets, pcap_handler func)
 
 void parsePacket(u_char *user, struct pcap_pkthdr *packethdr, u_char *packetptr)
 {
+    // reserved
+    //struct icmphdr* icmphdr; // for icmp
+    //struct tcphdr* tcphdr; // for tcp
+    //const struct sniff_ethernet *ethernet; // ethernet header
 
-
-
-
-    //struct ip* iphdr;
-    //struct icmphdr* icmphdr; // reserved for icmp
-    //struct tcphdr* tcphdr; // reserved for tcp
-    //struct udphdr* udphdr;
-    //const struct sniff_ethernet *ethernet;  /* The ethernet header [1] */
-    const struct sniff_ip *ip;              /* The IP header */
-    const struct sniff_udp *udp;            /* The UDP header */
-    const unsigned char *payload;                    /* Packet payload */
-
-
+    const struct sniff_ip *ip;              // IP header
+    const struct sniff_udp *udp;            // UDP header
+    const unsigned char *payload;           // payload of packet
 
     char iphdrInfo[256], srcip[256], dstip[256];
   //  unsigned short id, seq; // also reserved
-    int size_ip;
-    int size_payload;
+    int sizeIp;
+    int sizePayload;
 
-    /* define ethernet header */
+    // define ethernet header
    // ethernet = (struct sniff_ethernet*)(packetptr);
 
-    /* define/compute ip header offset */
+    // define/calc ip header offset
     ip = (struct sniff_ip*)(packetptr + SIZE_ETHERNET);
 
     // skip datalink layer header and get the IP header fields
-   // packetptr += linkhdrlen;
-    //iphdr = (struct ip*)packetptr;
     strcpy(srcip, inet_ntoa(ip->ip_src));
     strcpy(dstip, inet_ntoa(ip->ip_dst));
 
@@ -233,19 +223,18 @@ void parsePacket(u_char *user, struct pcap_pkthdr *packethdr, u_char *packetptr)
             4*ip->ip_vhl, ntohs(ip->ip_len));
     //printf("%s\n", iphdrInfo);
 
-    size_ip = IP_HL(ip)*4;
-    if (size_ip < 20) {
-        printf("   * Invalid IP header length: %u bytes\n", size_ip);
+    sizeIp = IP_HL(ip)*4;
+    if (sizeIp < 20) {
+        printf("   * Invalid IP header length: %u bytes\n", sizeIp);
         return;
     }
-
-    /* print source and destination IP addresses */
+    // reserved for all other proto
+    // print source and destination IP addresses
 /*    printf("       From: %s\n", inet_ntoa(ip->ip_src));
     printf("         To: %s\n", inet_ntoa(ip->ip_dst));*/
 
-    // Advance to the transport layer header then parse and display
-    // the fields based on the type of hearder: tcp, udp or icmp
-    //packetptr += 4*ip->ip_hl;
+    // parse and display the fields based on the type of hearder:
+    // tcp, udp or icmp
     switch (ip->ip_p)
     {
     //case IPPROTO_TCP:
@@ -267,38 +256,33 @@ void parsePacket(u_char *user, struct pcap_pkthdr *packethdr, u_char *packetptr)
     case IPPROTO_UDP:
     {
         printf("\n\n******************************\n\n");
-     /*   udphdr = (struct udphdr*)packetptr;
-        printf("UDP  %s:%d -> %s:%d\n", srcip, ntohs(udphdr->source),
-               dstip, ntohs(udphdr->dest));
-        printf("%s\n", iphdrInfo);
-*/
-        /* define/compute tcp header offset */
+        // define/compute udp header offset
         printf("       From: %s\n", inet_ntoa(ip->ip_src));
         printf("         To: %s\n", inet_ntoa(ip->ip_dst));
         udp = (struct sniff_udp*)(packetptr + SIZE_ETHERNET + SIZE_UDP);
         printf("   Src port: %d\n", ntohs(udp->uh_sport));
         printf("   Dst port: %d\n", ntohs(udp->uh_dport));
 
-        /* define/compute udp payload (segment) offset */
-        payload = (u_char *)(packetptr + SIZE_ETHERNET + size_ip + SIZE_UDP);
+        // define/compute udp payload (segment) offset
+        payload = (u_char *)(packetptr + SIZE_ETHERNET + sizeIp + SIZE_UDP);
 
-        /* compute udp payload (segment) size */
-        size_payload = ntohs(ip->ip_len) - (size_ip + SIZE_UDP);
-        if (size_payload > ntohs(udp->uh_ulen))
-            size_payload = ntohs(udp->uh_ulen);
+        // compute udp payload (segment) size
+        sizePayload = ntohs(ip->ip_len) - (sizeIp + SIZE_UDP);
+        if (sizePayload > ntohs(udp->uh_ulen))
+            sizePayload = ntohs(udp->uh_ulen);
 
-        /*
-        * Print payload data; it might be binary, so don't just
-        * treat it as a string.
-        */
-        if (size_payload > 0)
+
+        // print payload data;
+        // it might be binary
+        // but we treat it as a string.
+        if (sizePayload > 0)
         {
-            printf("   Payload (%d bytes):\n", size_payload);
-            print_payload(payload, size_payload);
+            printf("   Payload: (%d bytes):\n", sizePayload);
+            printPayload(payload, sizePayload);
         }
         break;
     }
-
+    // reserved
     /*case IPPROTO_ICMP:
         icmphdr = (struct icmphdr*)packetptr;
         printf("ICMP %s -> %s\n", srcip, dstip);
@@ -311,6 +295,7 @@ void parsePacket(u_char *user, struct pcap_pkthdr *packethdr, u_char *packetptr)
     }
 }
 // ala callback in windows
+/*
 void bailout(int signo)
 {
     struct pcap_stat stats;
@@ -323,101 +308,99 @@ void bailout(int signo)
     pcap_close(pd);
     exit(0);
 }
+*/
 
-/*
- * print packet payload data (avoid printing binary data)
- */
-void
-print_payload(const u_char *payload, int len)
+// print packet payload data (avoid printing binary data)
+void printPayload(const u_char *payload, int len)
 {
 
-    int len_rem = len;
-    int line_width = 16;            /* number of bytes per line */
-    int line_len;
-    int offset = 0;                 /* zero-based offset counter */
+    int lenRem = len;
+    int lineWidth = 16;            // number of bytes per line
+    int lineLen;
+    int offset = 0;                 // zero-based offset counter
     const u_char *ch = payload;
 
     if (len <= 0)
         return;
 
-    /* data fits on one line */
-    if (len <= line_width) {
-        print_hex_ascii_line(ch, len, offset);
+    // data fits on one line
+    if (len <= lineWidth) {
+        printHexAsciiLine(ch, len, offset);
         return;
     }
 
-    /* data spans multiple lines */
-    for ( ;; ) {
-        /* compute current line length */
-        line_len = line_width % len_rem;
-        /* print line */
-        print_hex_ascii_line(ch, line_len, offset);
-        /* compute total remaining */
-        len_rem = len_rem - line_len;
-        /* shift pointer to remaining bytes to print */
-        ch = ch + line_len;
-        /* add offset */
-        offset = offset + line_width;
-        /* check if we have line width chars or less */
-        if (len_rem <= line_width) {
-            /* print last line and get out */
-            print_hex_ascii_line(ch, len_rem, offset);
+    //data spans multiple lines
+    while(1)
+    {
+        // calc current line length
+        lineLen = lineWidth % lenRem;
+        // print line
+        printHexAsciiLine(ch, lineLen, offset);
+        // calc total remaining
+        lenRem = lenRem - lineLen;
+        // shift pointer to remaining bytes to print
+        ch = ch + lineLen;
+        // add offset
+        offset = offset + lineWidth;
+        //check if we have line width chars or less
+        if (lenRem <= lineWidth)
+        {
+            // print last line and get out
+            printHexAsciiLine(ch, lenRem, offset);
             break;
         }
     }
 
-return;
+    return;
 }
 
-/*
- * print data in rows of 16 bytes: offset   hex   ascii
- *
- * 00000   47 45 54 20 2f 20 48 54  54 50 2f 31 2e 31 0d 0a   GET / HTTP/1.1..
- */
-void
-print_hex_ascii_line(const u_char *payload, int len, int offset)
+// print data in rows of 16 bytes:
+// offset   hex   ascii
+// example:
+// 00000   47 45 54 20 2f 20 48 54  54 50 2f 31 2e 31 0d 0a   GET / HTTP/1.1..
+void printHexAsciiLine(const u_char *payload, int len, int offset)
 {
-
     int i;
     int gap;
     const u_char *ch;
 
-    /* offset */
-    printf("%05d   ", offset);
-
-    /* hex */
+    printf("%05d   ", offset); // offset
+    // hex
     ch = payload;
-    for(i = 0; i < len; i++) {
+    for(i = 0; i < len; i++)
+    {
         printf("%02x ", *ch);
         ch++;
-        /* print extra space after 8th byte for visual aid */
+        // print additional space after 8th byte for beautiful
         if (i == 7)
             printf(" ");
     }
-    /* print space to handle line less than 8 bytes */
+    // print space to handle line less than 8 bytes
     if (len < 8)
         printf(" ");
 
-    /* fill hex gap with spaces if not full line */
-    if (len < 16) {
+    // fill hex gap with spaces if not full line
+    if (len < 16)
+    {
         gap = 16 - len;
-        for (i = 0; i < gap; i++) {
+        for (i = 0; i < gap; i++)
+        {
             printf("   ");
         }
     }
     printf("   ");
 
-    /* ascii (if printable) */
+    // ascii (if printable)
     ch = payload;
-    for(i = 0; i < len; i++) {
+    for(i = 0; i < len; i++)
+    {
         if (isprint(*ch))
             printf("%c", *ch);
         else
             printf(".");
         ch++;
     }
-
     printf("\n");
 
-return;
+    return;
 }
--- sniffer.h
+++ sniffer.h
@@ -6,7 +6,7 @@
 void bailout(int signo);
 void parsePacket(u_char *user, struct pcap_pkthdr *packethdr, u_char *packetptr);
 void captureLoop(pcap_t* pd, int packets, pcap_handler func);
-void print_payload(const u_char *payload, int len);
-void print_hex_ascii_line(const u_char *payload, int len, int offset);
+void printPayload(const u_char *payload, int len);
+void printHexAsciiLine(const u_char *payload, int len, int offset);
 pcap_t* openPcapSocet(char *device, const char *bpfstr);
 #endif /* SNIFFER_H */
